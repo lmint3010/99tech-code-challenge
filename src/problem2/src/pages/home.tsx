43bb39b3cbf195307bf497cb4cd422f282edef03
lmint3010 <jeonkyosin@gmail.com>
2025-01-11 11:45:46 +0700

    style: responsive design

--- src/problem2/src/pages/home.tsx
+++ src/problem2/src/pages/home.tsx
@@ -5,12 +5,12 @@ import { CurrencySwapForm } from "@/components/currency-swap-form";
 
 export const HomePage: FC = () => {
 	return (
-		<div className="w-screen h-screen grid place-items-center bg-primary-background">
+		<div className="w-screen min-h-screen grid place-items-center bg-primary-background p-3">
 			<div className="flex flex-col items-center">
-				<div className="px-12 py-1 inline-block font-medium tracking-wider uppercase text-indigo-700 text-sm mb-4">
+				<div className="px-12 py-1 inline-block font-medium tracking-wider uppercase text-indigo-700 text-sm mb-4 w-full text-center">
 					Seamless currency exchange
 				</div>
-				<h1 className="text-4xl leading-tight font-medium text-center mb-12">
+				<h1 className="text-2xl sm:text-3xl md:text-4xl leading-tight font-medium text-center mb-12">
 					<span className="text-gray-700">Swap whenever and wherever</span>
 					<br />
 					<span className="bg-gradient-to-r from-indigo-600 to-primary-foreground bg-clip-text text-transparent">
